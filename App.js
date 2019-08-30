764e5de805a87d5e8ca322120b6414ce9ae7a08e
MUTUCare <mutucare@MUTUCares-MacBook-Pro.local>
2019-08-30 15:56:04 +0700

    add qrscan

--- App.js
+++ App.js
@@ -7,109 +7,138 @@
  */
 /*fariz*/
 
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React, { Component } from 'react';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { StyleSheet, View, Text, Platform, TouchableOpacity, Linking, PermissionsAndroid } from 'react-native';
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
+import { CameraKitCameraScreen, } from 'react-native-camera-kit';
 
+export default class App extends Component {
+  constructor() {
+
+    super();
+
+    this.state = {
+
+      QR_Code_Value: '',
+
+      Start_Scanner: false,
+
+    };
+  }
+
+  openLink_in_browser = () => {
+
+    Linking.openURL(this.state.QR_Code_Value);
+
+  }
+
+  onQR_Code_Scan_Done = (QR_Code) => {
+
+    this.setState({ QR_Code_Value: QR_Code });
+
+    this.setState({ Start_Scanner: false });
+  }
+
+  open_QR_Code_Scanner=()=> {
+
+    var that = this;
+
+    if (Platform.OS === 'android') {
+      async function requestCameraPermission() {
+        try {
+          const granted = await PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.CAMERA, {
+              'title': 'Camera App Permission',
+              'message': 'Camera App needs access to your camera '
+            }
+          )
+          if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+
+            that.setState({ QR_Code_Value: '' });
+            that.setState({ Start_Scanner: true });
+          } else {
+            alert("CAMERA permission denied");
+          }
+        } catch (err) {
+          alert("Camera permission err", err);
+          console.warn(err);
+        }
+      }
+      requestCameraPermission();
+    } else {
+      that.setState({ QR_Code_Value: '' });
+      that.setState({ Start_Scanner: true });
+    }
+  }
+  render() {
+    if (!this.state.Start_Scanner) {
+
+      return (
+        <View style={styles.MainContainer}>
+
+          <Text style={{ fontSize: 22, textAlign: 'center' }}>React Native Scan QR Code Example</Text>
+
+          <Text style={styles.QR_text}>
+            {this.state.QR_Code_Value ? 'Scanned QR Code: ' + this.state.QR_Code_Value : ''}
+          </Text>
+
+          {this.state.QR_Code_Value.includes("http") ?
+            <TouchableOpacity
+              onPress={this.openLink_in_browser}
+              style={styles.button}>
+              <Text style={{ color: '#FFF', fontSize: 14 }}>Open Link in default Browser</Text>
+            </TouchableOpacity> : null
+          }
+
+          <TouchableOpacity
+            onPress={this.open_QR_Code_Scanner}
+            style={styles.button}>
+            <Text style={{ color: '#FFF', fontSize: 14 }}>
+              Open QR Scanner
+            </Text>
+          </TouchableOpacity>
+
+        </View>
+      );
+    }
+    return (
+      <View style={{ flex: 1 }}>
+
+        <CameraKitCameraScreen
+          showFrame={true}
+          scanBarcode={true}
+          laserColor={'#FF3D00'}
+          frameColor={'#00C853'}
+          colorForScannerFrame={'black'}
+          onReadCode={event =>
+            this.onQR_Code_Scan_Done(event.nativeEvent.codeStringValue)
+          }
+        />
+
+      </View>
+    );
+  }
+}
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+
+  MainContainer: {
+    flex: 1,
+    paddingTop: (Platform.OS) === 'ios' ? 20 : 0,
+    alignItems: 'center',
+    justifyContent: 'center',
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  QR_text: {
+    color: '#000',
+    fontSize: 19,
+    padding: 8,
+    marginTop: 12
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  button: {
+    backgroundColor: '#2979FF',
+    alignItems: 'center',
+    padding: 12,
+    width: 300,
+    marginTop: 14
   },
 });
-
-export default App;
